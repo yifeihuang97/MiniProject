646b307b8da5c01a5771734e956a672fb74823d6
wusiqi1970 <siqiwu@bu.edu>
2018-09-11 15:28:40 -0400

    Login

---
+++ Mini Project/Mini Project/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mini Project
+//
+//  Created by David wu on 9/10/18.
+//  Copyright © 2018 David wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
